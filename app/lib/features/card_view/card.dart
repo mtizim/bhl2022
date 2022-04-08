3c20e46c7a89bd4ed18b1bc7b9d33f9d89d2e122
Mateusz Ziemła <mtizim1@gmail.com>
2022-04-08 20:45:04 +0200

    card client start

---
+++ app/lib/features/card_view/card.dart
@@ -0,0 +1,16 @@
+import 'package:app/features/card_view/cardmanager.dart';
+import 'package:flutter/material.dart';
+
+class CardView extends StatelessWidget {
+  const CardView({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+
+  final CardData data;
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
--- app/lib/features/card_view/cardmanager.dart
+++ app/lib/features/card_view/cardmanager.dart
@@ -1,3 +1,67 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-// part 'cardmanager.freezed.dart';
+part 'cardmanager.freezed.dart';
+
+class CardManager extends Cubit<CardManagerState> {
+  CardManager() : super(const CardManagerState.loading());
+
+  void fetch() async {
+    // TODO get data from server
+    await Future<void>.delayed(Duration(milliseconds: 300));
+  }
+
+  void _requestMoreData() async {
+    // TODo get data from server
+    await Future<void>.delayed(const Duration(milliseconds: 300));
+  }
+
+  void _onNext() {
+    final newState = state.map(
+      loading: (lo) => lo,
+      loaded: (s) {
+        if (s.data.length - s.offset < 4) {
+          _requestMoreData();
+        }
+        return s;
+      },
+    );
+
+    emit(newState);
+  }
+
+  void interested() {
+    _onNext();
+  }
+
+  void uninterested() {
+    _onNext();
+  }
+}
+
+@freezed
+class CardManagerState with _$CardManagerState {
+  const factory CardManagerState.loading() = CardManagerLoadingstate;
+  const factory CardManagerState.loaded({
+    required List<CardData> data,
+    required int offset,
+  }) = CardManagerLoadedState;
+}
+
+CardData? parseData(dynamic data) {
+  print(data);
+  return null;
+}
+
+@freezed
+class CardData with _$CardData {
+  factory CardData({
+    required int minCapacity,
+    required int cost,
+    required String address,
+    required List<String> tags,
+    required String name,
+    required String description,
+    required Uri imageLink,
+  }) = _CardData;
+}
